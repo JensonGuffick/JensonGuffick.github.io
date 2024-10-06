59d6fec64ea72d41c459d1557c3fc4ee44d2d226
Jens <jensonguffick@yahoo.com>
2024-10-06 14:50:44 +0100

    Initial commit

---
+++ LPBMBookings.php
@@ -0,0 +1,181 @@
+<?php
+
+###############################################################
+# Page Password Protect 2.13
+###############################################################
+# Visit http://www.zubrag.com/scripts/ for updates
+############################################################### 
+#
+# Usage:
+# Set usernames / passwords below between SETTINGS START and SETTINGS END.
+# Open it in browser with "help" parameter to get the code
+# to add to all files being protected. 
+#    Example: password_protect.php?help
+# Include protection string which it gave you into every file that needs to be protected
+#
+# Add following HTML code to your page where you want to have logout link
+# <a href="http://www.example.com/path/to/protected/page.php?logout=1">Logout</a>
+#
+###############################################################
+
+/*
+-------------------------------------------------------------------
+SAMPLE if you only want to request login and password on login form.
+Each row represents different user.
+
+$LOGIN_INFORMATION = array(
+  'zubrag' => 'root',
+  'test' => 'testpass',
+  'admin' => 'passwd'
+);
+
+--------------------------------------------------------------------
+SAMPLE if you only want to request only password on login form.
+Note: only passwords are listed
+
+$LOGIN_INFORMATION = array(
+  'root',
+  'testpass',
+  'passwd'
+);
+
+--------------------------------------------------------------------
+*/
+
+##################################################################
+#  SETTINGS START
+##################################################################
+
+// Add login/password pairs below, like described above
+// NOTE: all rows except last must have comma "," at the end of line
+$LOGIN_INFORMATION = array(
+  'zubrag' => 'root',
+  'admin' => 'adminpass'
+);
+
+// request login? true - show login and password boxes, false - password box only
+define('USE_USERNAME', true);
+
+// User will be redirected to this page after logout
+define('LOGOUT_URL', 'http://www.example.com/');
+
+// time out after NN minutes of inactivity. Set to 0 to not timeout
+define('TIMEOUT_MINUTES', 0);
+
+// This parameter is only useful when TIMEOUT_MINUTES is not zero
+// true - timeout time from last activity, false - timeout time from login
+define('TIMEOUT_CHECK_ACTIVITY', true);
+
+
+// show usage example
+if(isset($_GET['help'])) {
+  die('Include following code into every page you would like to protect, at the very beginning (first line):<br>&lt;?php include("' . str_replace('\\','\\\\',__FILE__) . '"); ?&gt;');
+}
+
+// timeout in seconds
+$timeout = (TIMEOUT_MINUTES == 0 ? 0 : time() + TIMEOUT_MINUTES * 60);
+
+// logout?
+if(isset($_GET['logout'])) {
+  setcookie("verify", '', $timeout, '/'); // clear password;
+  header('Location: ' . LOGOUT_URL);
+  exit();
+}
+
+if(!function_exists('showLoginPasswordProtect')) {
+
+function showLoginPasswordProtect($error_msg) {
+?>
+<html>
+<head>
+  <title>Enter Password</title>
+  <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+  <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+</head>
+<body>
+  <style>
+    body {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      background-color: #ffcc00;
+      font-size: 2em;
+      text-align: center;
+      display: grid;
+    }
+    input {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      text-align: center;
+      border: 1px solid black;
+      border-radius: 50px;
+      width: 300px;
+      font-size: 1em;
+      box-shadow: 0 0 10px #000000;
+    }
+  </style>
+  <div style="text-align:center">
+    <form method="post">
+      <h3><b>Please enter password to access this page</b></h3>
+      <a color="red"></a>
+      <br>
+      <b>Login:</b>
+      <br>
+      <input type="input" name="access_login">
+      <br>
+      <b>Password:</a>
+      <br>
+      <input type="password" name="access_password">
+      <br>
+      <br>
+      <input type="submit" name="Submit" value="Submit">
+    </form>
+    <br>
+  </div>
+</body>
+</html>
+
+<?php
+  die();
+}
+}
+
+if (isset($_POST['access_password'])) {
+
+  $login = isset($_POST['access_login']) ? $_POST['access_login'] : '';
+  $pass = $_POST['access_password'];
+  if (!USE_USERNAME && !in_array($pass, $LOGIN_INFORMATION)
+  || (USE_USERNAME && ( !array_key_exists($login, $LOGIN_INFORMATION) || $LOGIN_INFORMATION[$login] != $pass ) ) 
+  ) {
+    showLoginPasswordProtect("Incorrect password.");
+  }
+  else {
+    setcookie("verify", md5($login.'%'.$pass), $timeout, '/');
+    unset($_POST['access_login']);
+    unset($_POST['access_password']);
+    unset($_POST['Submit']);
+  }
+
+}
+
+else {
+  if (!isset($_COOKIE['verify'])) {
+    showLoginPasswordProtect("");
+  }
+
+
+  $found = false;
+  foreach($LOGIN_INFORMATION as $key=>$val) {
+    $lp = (USE_USERNAME ? $key : '') .'%'.$val;
+    if ($_COOKIE['verify'] == md5($lp)) {
+      $found = true;
+      if (TIMEOUT_CHECK_ACTIVITY) {
+        setcookie("verify", md5($lp), $timeout, '/');
+      }
+      break;
+    }
+  }
+  if (!$found) {
+    showLoginPasswordProtect("");
+  }
+
+}
+
+?>
