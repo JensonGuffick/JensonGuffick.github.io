25e8405cc70886a62744e594d9c49eaf03c11600
Jens <jensonguffick@yahoo.com>
2024-10-30 13:51:01 +0000

    Changes

--- Scripts/PasswordLocker.js
+++ Scripts/PasswordLocker.js
@@ -14,6 +14,8 @@ function checkLogin() {
             if (loginValue === 'member' || loginValue === 'beginner') {
                 console.log('User logged in, Passing through');
                 loggedIn = true;
+                console.log('-----------------------------');
+                renewCookie();
                 break;
             }
         }
@@ -23,6 +25,27 @@ function checkLogin() {
         console.log('No Users logged in, Redirecting.');
         window.location.href = '/auth/LoginSelector.html';
     }
+}
+
+function renewCookie() {
+    console.log('Renewing Cookie');
+    const now = new Date();
+    const expires = new Date(now.getTime() + 15 * 60 * 1000);
+    document.cookie = `login=${getCurrentLoginValue()}; expires=${expires.toUTCString()}; path=/`;
+    console.log('Successfully Renewed');
+    console.log('-----------------------------');
+    console.log('Exiting.');
+}
+
+function getCurrentLoginValue() {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+        const trimmedCookie = cookie.trim();
+        if (trimmedCookie.startsWith('login=')) {
+            return trimmedCookie.split('=')[1];
+        }
+    }
+    return '';
 }
 
 checkLogin();
